e505fdafed58d61ec3f5b0a6c1c2acc705d678a3
Serhiy21 <72924537+Serhiy21@users.noreply.github.com>
2020-10-27 16:59:55 -0400

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,34 @@
+publuc class Main {
+  public static void main(String[] args)  {
+
+    //invoke various methods that reveals details about yourself
+    myName();
+    myAge();
+    myFavorites();
+    mySummerPlans();
+
+  }
+
+  /*
+   * for each of the following methods:
+   * include relevant details that apply to the method header.
+   * you may simply write print statements in the method OR
+   * you may choose to change the return type from void to whatever is appropriate.
+   */
+  public static void myName(){
+
+  }
+
+  public static void myAge(){
+
+  }
+
+  public static void myFavorites(){
+
+  }
+
+  public static void mySummerPlans(){
+
+  }
+
+}
