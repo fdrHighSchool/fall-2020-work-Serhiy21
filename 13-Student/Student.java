890599b1a4e50bb2ecb1482b1d23d6981a717bca
Serhiy21 <72924537+Serhiy21@users.noreply.github.com>
2021-01-07 22:21:11 -0500

    Create Student.java

---
+++ 13-Student/Student.java
@@ -0,0 +1,37 @@
+public class Student {
+    // instance varaibles
+ 
+    private String name;
+    private String osis;
+    private int[] grades;
+
+    // constructor
+
+
+    // other methods
+
+
+    public String getName(){
+
+    } //end of getName method
+
+
+    public String getOsis(){
+
+    } //end of getOsis method
+
+    public void setName(){
+
+    } //end of setName method
+
+    public void addGrades(int arr) {
+
+
+    } //end addGrades()
+
+    public double calculateAverage(){
+
+    } //end of calculateAverage method
+
+
+}//end class
