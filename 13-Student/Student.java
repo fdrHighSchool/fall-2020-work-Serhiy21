839a06eb230f8e41c5e90ab173d17080ff819384
Serhiy21 <72924537+Serhiy21@users.noreply.github.com>
2021-01-19 17:02:17 -0500

    Update Student.java

--- 13-Student/Student.java
+++ 13-Student/Student.java
@@ -1,78 +1,31 @@
-import java.util.*;
-
 public class Student {
-  // instance variables
-  private String name;
-  private String osis;
-  private int[] grades;
-  private int numGrades;
+	   private String name;
+	   private int number; 
+	   private int age;
+	   private String gender; 
+	   private int[] grade;
+	   
+	   public Student(String stuName,int stuNumber,int stuAge,String stuGender, int[] stuGrade){
+				this.name = stuName;
+				this.number = stuNumber;
+				this.gender = stuGender;
+				this.age = stuAge;
+				this.grade=stuGrade;
+			}
 
-  // constructor
-  public Student(String n, String o) {
-    this.name = n;
-    this.osis = o;
-    this.grades = new int[10];
-    this.numGrades = 0;
-  } //end Student()
-
-  // other methods
-
-  public String getName() {
-    return this.name;
-  } //end getName()
-
-  public String getOSIS() {
-    return this.osis;
-  } //end getOSIS()
-
-  public void displayGrades() {
-    System.out.println(Arrays.toString(this.grades));
-  } //end getGrades()
-
-  public void setName(String name) {
-    this.name = name;
-  } //end setName()
-
-  public void fillArray() {
-    for (int i = 0; i < this.grades.length; i++) {
-      this.grades[i] = (int)(Math.random() * 46 + 55);
-    }// end for loop
-    this.numGrades = 10;
-  }// end fillArray method
-
-  public void addGrades(int grade) {
-    //if there's no room, then replace the lowest score
-    if (this.numGrades == 10) {
-
-    } //end if
-    this.grades[this.numGrades] = grade;
-    this.numGrades++;
-  } //end addGrades()
-
-  public double calculateAverage() {
-    double sum = 0.0;
-    int count = 0;
-
-    //VERSION 1 - loop through entire array always
-    /*
-    for (int i = 0; i < this.grades.length; i++) {
-      if (this.grades[i] != 0) {
-        sum += this.grades[i];
-        count++;
-      } //end if value if not 0
-    } //end for loop
-    */
-
-    //VERSION 2 - use numGrades to determine how many iterations through the
-    for (int i = 0; i < this.numGrades; i++) {
-      sum += this.grades[i];
-    } //end for loop
-
-    return sum / this.numGrades;
-  } //end calculateAverage()
-
-  public String toString() {
-    return "Student:" + this.name + " OSIS:" + this.osis;
-  } //end toString()
-
-} //end class
+	   public String getName(){
+	        return name;
+	   }
+	   public int getNumber(){
+	        return number;
+	   }
+	   public int getAge(){
+	        return age;
+	   }
+	   public String getGender(){
+	        return gender;
+	   }
+	   public int[] getGrade(){
+	        return grade;
+	   }
+}
