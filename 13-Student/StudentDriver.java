85eddc4851e2c232f54655802b766eac9083a25f
Serhiy21 <72924537+Serhiy21@users.noreply.github.com>
2021-01-19 17:24:41 -0500

    Update StudentDriver.java

--- 13-Student/StudentDriver.java
+++ 13-Student/StudentDriver.java
@@ -2,28 +2,36 @@ import java.util.*;
 
 public class StudentDriver {
   public static void main(String[] args) {
-    Student student1 = new Student("Ma", "239970163");
-    Student student2 = new Student("Hello", "1266874531");
+    Student student1 = new Student("Artur", "938472019");
+    Student student2 = new Student("Anna", "648593748");
     System.out.println(student1);
     System.out.println(student2);
 
-    student1.setName("Jinglun");
+    student1.setName("Artur");
     System.out.println(student1);
 
     System.out.println(student1.getName());
-    System.out.println(student1.getOSIS());
+    System.out.println(student1.getNumber());
 
-    student1.addGrades(100);
-    student1.displayGrades();
-    student1.addGrades(90);
-    student1.displayGrades();
+    student1.addGender("Male");
+    student1.displayGender();
+    student1.addAge(15);
+    student1.displayAge();
+    student1.addGrade(75);
+    student1.displayGrade();
 
-    student2.addGrades(95);
-    student2.displayGrades();
+    student2.addGender("Female");
+    student2.displayGender();
+    student2.addAge(16);
+    student2.displayAge();
+    student2.addGrade(80);
+    student2.displayGrade();
 
-    Student student3 = new Student("Siri", "987654321");
+    Student student3 = new Student("Clara", "236784096");
     student3.fillArray();
-    student3.displayGrades();
+    student3.displayGender();
+    student3.displayAge();
+    student3.displayGrade();
     System.out.println(student1.calculateAverage());
     System.out.println(student2.calculateAverage());
     System.out.println(student3.calculateAverage());
